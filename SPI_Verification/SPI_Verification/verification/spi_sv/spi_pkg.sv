7955b3f6a528014d7db69aba80d5e6084738ee4e
Waleed53 <127500143+Waleed53@users.noreply.github.com>
2026-04-18 20:52:24 +0500

    Add SPI UVM Verification: SPI_Verification/SPI_Verification/verification/spi_sv/spi_pkg.sv

---
+++ SPI_Verification/SPI_Verification/verification/spi_sv/spi_pkg.sv
@@ -0,0 +1,16 @@
+package spi_pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "spi_transaction.sv"
+  `include "spi_monitor.sv"
+  `include "spi_sequencer.sv"
+  `include "spi_seqs.sv"
+  `include "spi_driver.sv"
+  `include "spi_agent.sv"
+  `include "spi_environment.sv"
+
+
+endpackage
+
