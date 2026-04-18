1139042472c5e81e811817f42757a6f3786e9e7d
Waleed53 <127500143+Waleed53@users.noreply.github.com>
2026-04-18 20:52:48 +0500

    Add SPI UVM Verification: SPI_Verification/SPI_Verification/verification/wishbone_sv/wb_sequencer.sv

---
+++ SPI_Verification/SPI_Verification/verification/wishbone_sv/wb_sequencer.sv
@@ -0,0 +1,20 @@
+
+class wb_sequencer extends uvm_sequencer #(wb_spi_transaction);
+
+    `uvm_component_utils(wb_sequencer)
+
+    function new(string name, uvm_component parent);
+    
+        super.new(name, parent);
+        
+    endfunction
+    
+    virtual function void start_of_simulation_phase(uvm_phase phase);
+    
+        super.start_of_simulation_phase(phase);
+        `uvm_info(get_type_name(), "\n\nRunning Simulation in wb_sequencer", UVM_HIGH)
+        
+    endfunction : start_of_simulation_phase
+
+
+endclass : wb_sequencer
