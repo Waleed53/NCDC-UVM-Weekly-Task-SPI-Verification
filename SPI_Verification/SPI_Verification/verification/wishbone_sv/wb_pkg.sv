e4096976a66c4f89dded491918207c5402c974a8
Waleed53 <127500143+Waleed53@users.noreply.github.com>
2026-04-18 20:52:46 +0500

    Add SPI UVM Verification: SPI_Verification/SPI_Verification/verification/wishbone_sv/wb_pkg.sv

---
+++ SPI_Verification/SPI_Verification/verification/wishbone_sv/wb_pkg.sv
@@ -0,0 +1,14 @@
+package wb_pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "wb_spi_transaction.sv"
+  `include "wb_monitor.sv"
+  `include "wb_sequencer.sv"
+  `include "wb_seqs.sv"
+  `include "wb_driver.sv"
+  `include "wb_agent.sv"
+  `include "wb_environment.sv"
+
+endpackage
